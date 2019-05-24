856a8f1474d96d86977dd34bcfcd789d7fe90e2e
ChetnaSingh <chetnasingh03052312@gmail.com>
2019-05-24 21:39:11 +0530

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: [
+    './main.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'www'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+};
