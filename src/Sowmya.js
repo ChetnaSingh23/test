b7cf753617f45e02e0f110dc336db26c74508e3e
Sowmya Ganesh Gupta <sowmya.bvm@gmail.com>
2019-05-28 22:14:45 -0700

    Completed task

--- src/Sowmya.js
+++ src/Sowmya.js
@@ -20,7 +20,14 @@ class Sowmya extends React.Component {
     return (
       <div>
         <h2>Sowmya</h2>
-
+        <a href="https://www.google.com">Google</a><br/>
+        <p>This is a paragraph.</p><br/>
+        <label for="color_example">Color:</label>
+        <input type="color" name="color_example" /> <br />
+        <label for="date_and_time">Date with time:</label>
+        <input type="datetime-local" name="date_and_time" /><br />
+        <label for="week">Week:</label>
+        <input type="week" name="week" /><br />
       </div>
       
     );
